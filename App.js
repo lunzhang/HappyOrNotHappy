dedf140ed5cc08066aee9922254f3953f05e0ed2
lunzhang <lun.zhang1993@gmail.com>
2019-05-23 15:28:47 -0400

    Use the highest camera ratio

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Camera, Permissions } from 'expo';
 
-export default class App extends React.Component {
-  componentDidMount() {
-    this.askPermission();
-  }
+export default class App extends Component {
+  state = { ratio: '1:1' }
 
   async askPermission() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -14,11 +12,19 @@ export default class App extends React.Component {
     }
   }
 
+  async componentDidMount() {
+    this.askPermission();
+    const ratio = await this.camera.getSupportedRatiosAsync();
+    this.setState({ ratio: ratio[ratio.length - 1] });
+  }
+
   render() {
     return (
       <Camera
         style={{ flex: 1 }}
         type={Camera.Constants.Type.front}
+        ratio={this.state.ratio}
+        ref={ref => { this.camera = ref; }}
       />
     );
   }
